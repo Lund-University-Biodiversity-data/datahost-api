0f268fe80db9bc2f44310a9beb063bc236b6296e
mathieuLU <you@example.com>
2023-01-12 18:35:23 +0100

    config template

--- config/config_template.js
+++ config/config_template.js
@@ -5,10 +5,15 @@ config.apiServerUrl='http://URL';
 config.SLUAPIkey = "YOUR_KEY_HERE";
 config.speciesFilePath = "PATH";
 config.speciesHierarchyFilePath = "PATH";
-config.urlAPIListsALABirds = "URL";
+
 config.urlAPISLUdetails = "URLwith{taxonId}andyourkey=";+config.SLUAPIkey;
 config.urlAPISLUgetTaxa = "URLwith{taxonId}andyourkey=";+config.SLUAPIkey;
-config.dyntaxaIdAves=4000104;
+
+config.urlAPILists=[
+	"https://lists.biodiversitydata.se/ws/URL", // birds
+	"https://lists.biodiversitydata.se/ws/URL" // mammals
+];
+config.dyntaxaIdFinalStoppingTaxon=[ 123456 ];
 
 // For YAML file generation
 config.folderOpenApiFiles = "folder/";
